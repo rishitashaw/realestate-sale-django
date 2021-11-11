06a3cb0c067a82bcb1b55f3221112b0ab211226d
Theseregrets <rishitashaw49@gmail.com>
2021-11-11 19:38:56 +0530

    html added

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
