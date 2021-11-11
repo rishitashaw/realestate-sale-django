9f3877ea547af2bb965d8a3bccc1b01029a1a865
Theseregrets <rishitashaw49@gmail.com>
2021-11-11 19:44:00 +0530

    base added

--- pages/views.py
+++ pages/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'pages/index.html')
 
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'pages/about.html')
