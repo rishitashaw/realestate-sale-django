e4227d0efb579a8dc50e8469f168346042903e7c
theseregrets <rishitashaw49@gmail.com>
2021-11-19 19:56:06 +0530

    connected to postgres

--- btre/settings.py
+++ btre/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages.apps.PagesConfig'
+    'pages.apps.PagesConfig',
+    'listing.apps.ListingConfig',
+    'realtors.apps.RealtorsConfig'
 ]
 
 MIDDLEWARE = [
@@ -77,8 +79,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dtredb',
+        'USER': 'postgres',
+        'PASSWORD': 'Harvard123',
+        'HOST': 'localhost',
     }
 }
 
--- listing/views.py
+++ listing/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'listing/listings.html')
+
+
+def listing(request):
+    return render(request, 'listing/listing.html')
+
+
+def search(request):
+    return render(request, 'listing/search.html')
