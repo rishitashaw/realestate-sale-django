5beb0231595f6cefbf5dfd917a88dad7929f1dee
theseregrets <rishitashaw49@gmail.com>
2021-11-21 23:30:04 +0530

    admin section changed

--- listing/models.py
+++ listing/models.py
@@ -6,7 +6,6 @@ from realtors.models import Realtor
 
 class Listing(models.Model):
     realtors = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
-    id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=100)
