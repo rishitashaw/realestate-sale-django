3384b8531e96147824649e3f36b6eb5f0e336117
theseregrets <rishitashaw49@gmail.com>
2021-11-23 21:25:49 +0530

    admin page changed

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Contact
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'listing', 'contact_date')
+
+
+admin.site.register(Contact, ContactAdmin)
