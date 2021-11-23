5dd1449c53d27d2d2dce49edd48922082599c68a
theseregrets <rishitashaw49@gmail.com>
2021-11-23 22:35:02 +0530

    secrets removed

--- btre/settings.py
+++ btre/settings.py
@@ -84,9 +84,9 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dtredb',
+        'NAME': '',
         'USER': 'postgres',
-        'PASSWORD': 'Harvard123',
+        'PASSWORD': '',
         'HOST': 'localhost',
     }
 }
