66da6844316c1e917fef998ca3ccdd24fec3c988
theseregrets <rishitashaw49@gmail.com>
2021-11-23 18:23:41 +0530

    all info rendered

--- listing/views.py
+++ listing/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Listing
+from realtors.models import Realtor
 # Create your views here.
 
 
 def index(request):
     listings = Listing.objects.order_by(
         '-list_date').filter(is_published=True).all()
-    paginator = Paginator(listings, 3)
+    paginator = Paginator(listings, 6)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
     context = {
@@ -18,7 +19,12 @@ def index(request):
 
 
 def listing(request, listing_id):
-    return render(request, 'listing/listing.html')
+    listing = Listing.objects.get(id=listing_id)
+    context = {
+        'listing': listing,
+
+    }
+    return render(request, 'listing/listing.html', context)
 
 
 def search(request):
--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from listing.models import Listing
+from realtors.models import Realtor
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by(
+        '-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings': listings,
+    }
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')[:3]
+    realtor_mvp = Realtor.objects.filter(is_mvp=True).first()
+
+    context = {'realtors': realtors, 'realtor_mvp': realtor_mvp}
+    return render(request, 'pages/about.html', context)
